f54c9cd87eec6b5e26558da2474478b9caa289e8
Stefan <stefan@glasscanvascreative.com>
2014-12-10 08:04:40 -0800

    Re-organize JS (from serena to glass & canvas)

--- app/assets/javascripts/refinery-glass.js
+++ app/assets/javascripts/refinery-glass.js
@@ -1,8 +1,5 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
+// This is a manifest file that'll be compiled into refinery-glass.js, which will include all the files
 // listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
 //
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // the compiled file.
@@ -10,7 +7,11 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require jquery.form.min
-//= require modules/forms.js
-//= require modules/main.js
-//= require modules/modal-create.js
+//  # canvas/bootstrap not needed??
+//= require canvas/jquery.form.min
+//= require glass/semantic-ui/sidebar.js
+//= require glass/semantic-ui/index.js
+//= require canvas/modules/forms.js
+//= require canvas/modules/main.js
+//= require glass/modules/menus.js
+//= require glass/modules/modal-create.js
