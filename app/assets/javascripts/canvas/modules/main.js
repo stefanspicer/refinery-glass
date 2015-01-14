9411e67d2456d30bf52c0d311a0c09c9d6784316
Joseph Krump <krump.joseph@gmail.com>
2015-01-14 00:00:15 -0800

    added a method that can take a DOM node and get a unique selector for that item

--- app/assets/javascripts/canvas/modules/main.js
+++ app/assets/javascripts/canvas/modules/main.js
@@ -5,12 +5,36 @@ var Main = (function($){
     });
   });
 
+  var getUniqueSelector = function (node) {
+    if (node.length != 1) throw 'Requires one element.';
+
+    var path;
+    while (node.length) {
+      var realNode = node[0], name = realNode.localName;
+      if (!name) break;
+      name = name.toLowerCase();
+
+      var parent = node.parent();
+
+      var siblings = parent.children(name);
+      if (siblings.length > 1) {
+        name += ':eq(' + siblings.index(realNode) + ')';
+      }
+
+      path = name + (path ? '>' + path : '');
+      node = parent;
+    }
+
+    return path;
+  };
+
   function contentReady(element) {
     $(document).trigger('content-ready', element);
   }
 
-  //Return API for other modules
+  // Return API for other modules
   return {
+    getUniqueSelector: getUniqueSelector,
     contentReady: contentReady
   };
 })(jQuery);
