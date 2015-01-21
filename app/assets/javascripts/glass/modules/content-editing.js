ab1b6c60da7a7794d84a76c8938e341852752edf
Stefan <stefan@glasscanvascreative.com>
2015-01-21 12:42:22 -0800

    Refactor live html editor - remove div wrappers
     - Also refactored for a better overall interface using jquery & js OOP

--- app/assets/javascripts/glass/modules/content-editing.js
+++ app/assets/javascripts/glass/modules/content-editing.js
@@ -14,140 +14,274 @@ var GlassContentEditing = (function ($) {
     });
   });
 
+  $.fn.extend({
+    // A classChunk is an editable block that will be saved to the DB
+    glassChunk: function () {
+      var chunk = this.data('glass-chunk');
+      if (!chunk) {
+        chunk = new GlassChunk(this);
+        this.data('glass-chunk', chunk);
+      }
+      return chunk;
+    },
+    // A glassHtmlEditor is a block of editable html (with modules)
+    glassHtmlEditor: function () {
+      var html = this.data('glass-html');
+      if (!html) {
+        html = new GlassHtmlEditor(this);
+        this.data('glass-html', html);
+      }
+      return html;
+    },
+    // A glassModule is a piece within a glassHtmlEditor block
+    glassHtmlModule: function ($glass_html_editor) {
+      var module = this.data('glass-module');
+      if (!module) {
+        module = new GlassModule(this, $glass_html_editor);
+        this.data('glass-module', module);
+      }
+      return module;
+    },
+    // A glassControl is an action (or set of actions) to be made on a module
+    glassHtmlControl: function () {
+      var control = this.data('glass-control');
+      if (!control) {
+        control = new GlassControl(this);
+        this.data('glass-control', control);
+      }
+      return control;
+    },
+  });
 
   // #############################################################
-  // # Save & Retrieve                                           #
+  // # Save & Retrieve to DB (via hidden form)                   #
   // #############################################################
+
+  function GlassChunk($elem) {
+    this.ch = {'elem': $elem};
+
+    // Return the glass editing options (default is 'text', like a fancy <input type="text">)
+    this.options = function() {
+      if (!this.ch.options) {
+        var opt_str = this.ch.elem.attr('data-glass-options');
+        this.ch.options = $.extend({'type': 'text'}, opt_str ? JSON.parse(opt_str) : {});
+      }
+      return this.ch.options;
+    };
+
+    this.option = function(key) {
+      return (key in this.options()) ? this.options()[key] : null;
+    };
+
+    this.getFormElement = function() {
+      if (!this.ch.form_elem) {
+        this.ch.form_elem = this.option('form_id') ? $('#' + this.option('form_id')) : null;
+      }
+      return this.ch.form_elem;
+    };
+
+    this.getForm = function() {
+      var $form_elem = this.getFormElement();
+      return $form_elem ? $form_elem.parents('form') : null;
+    };
+
+    this.setFormValFromHtml = function() {
+      var $form_elem = this.getFormElement();
+      if ($form_elem) {
+        if (this.option('type') == 'text') {
+          $form_elem.val(this.ch.elem.html());
+        }
+        else if (this.option('type') == 'html') {
+          $form_elem.val(this.ch.editor.formatHtml());
+        }
+      }
+    };
+
+    this.makeEditable = function() {
+      if (this.option('type') == 'text') {
+        this.ch.elem.attr('contenteditable', true);
+      }
+      else if (this.option('type') == 'html') {
+        this.ch.editor = this.ch.elem.glassHtmlEditor();
+      }
+    }
+  }
+
+  // #############################################################
+  // # HTML editor (actually manipulate the html)                #
+  // #############################################################
+  function GlassHtmlEditor($elem) {
+    var this_editor = this;
+    this.h = {'elem': $elem, 'control': {}, 'control_stack': []};
+
+    this.control = function(key) {
+      return this.h.control[key];
+    };
+
+    this.curModule = function() {
+      return this.h.cur_module;
+    };
+
+    this.setCurModule = function($module) {
+      this.h.cur_module = $module;
+      this.hideControl();
+    };
+
+    this.attachControl = function(key) {
+      var $control = this.control(key);
+      var stack = this.h.control_stack;
+      $control.attachToModule(this.curModule());
+      if (stack.length > 0) {
+        stack[stack.length - 1].element().hide();
+      }
+      stack.push($control);
+    };
+
+    this.closeCurControl = function() {
+      var stack = this.h.control_stack;
+      stack.pop().element().hide();
+      stack[stack.length - 1].element().fadeIn();
+    };
+
+    this.moduleBefore = function($module) {
+      var $prev_elem = $module.element().prev('[contenteditable=true]');
+      return ($prev_elem && $prev_elem.data('glass-module')) ? $prev_elem.glassHtmlModule(this_editor) : null;
+    };
+
+    this.insertNewModuleAfter = function($hook) {
+      var $new_para = $('<p/>', { placeholder: 'New Paragraph...' });
+      $hook.insertAfter("\n  ");
+      $new_para.insertAfter($hook);
+      $(document).trigger('content-ready', $new_para[0]);
+      var $new_module = $new_para.glassHtmlModule(this_editor);
+      this.attachControl('change_module');
+      return $new_module;
+    };
+
+    this.hideControl = function() {
+      this.h.elem.find('.glass-control').appendTo('#glass-parking');
+      this.h.control_stack = [];
+    };
+
+    this.formatHtml = function() {
+      this.h.elem.find('[contenteditable=true]').removeAttr('contenteditable');
+      this.hideControl();
+      return this.h.elem.html().trim();
+    };
+
+    // Initialization
+    // ##########################################
+    this.h.elem.find('p, h1, h2, h3, h4, h5, h6').each(function () {
+      if ($(this).parents('.glass-no-edit').length == 0) {
+        // new GlassModule ??
+        $(this).glassHtmlModule(this_editor);
+      }
+    });
+
+    this.h.control['change_module'] = $('#glass-change-module').glassHtmlControl();
+    this.h.control['choose_module'] = $('#glass-choose-module').glassHtmlControl();
+    this.h.control['settings_vid']  = $('#glass-module-settings-vid').glassHtmlControl();
+    //this.h['control']['change_module'] = $('#glass-change-module').glassHtmlControl();
+    //this.h['control']['choose_module'] = $('#glass-choose-module').glassHtmlControl();
+    //this.h['control']['settings_vid']  = $('#glass-module-settings-vid').glassHtmlControl();
+
+    $('#glass-change-module').click(function (e) {
+      e.preventDefault();
+      this_editor.attachControl('choose_module');
+    });
+
+    $('#glass-choose-module-vid').click(function (e) {
+      e.preventDefault();
+      this_editor.attachControl('settings_vid');
+    });
+
+    $('.glass-control-close').click(function (e) {
+      e.preventDefault();
+      this_editor.closeCurControl();
+    });
+  }
+
+  // #############################################################
+  // # Module - a paragraph, heading, img, video...              #
+  // #############################################################
+  function GlassModule($elem, $editor) {
+    var this_module = this;
+    this.m = {'elem': $elem, 'editor': $editor};
+
+    this.focus = function() {
+      //$new_module.find('p').first().focus();
+      this.m.elem.focus();
+      this.m.editor.setCurModule(this);
+    };
+
+    this.element = function() {
+      return this.m.elem;
+    };
+
+    this.remove = function() {
+      this.m.elem.remove();
+    }
+
+    // Initialization
+    // ##########################################
+    this.m.elem.attr('contenteditable', true);
+    this.m.elem.focusout(function () {
+      if (!$(this).text().trim()) {
+        $(this).html(''); // For FF, it puts a <br type='-moz'> in there
+      }
+    });
+    this.m.elem.keypress(function(e) {
+      if (e.which == 13) {
+        e.preventDefault();
+        var $new_module = this_module.m.editor.insertNewModuleAfter($(this));
+      }
+
+      if (e.which == 8 && !$(this).text().trim()) {
+        e.preventDefault();
+        this_module.m.editor.hideControl();
+        this_module.m.editor.moduleBefore(this_module).focus();
+        this_module.remove();
+      }
+    });
+    this.focus();
+  }
+
+  // #############################################################
+  // # Control - actions on a module (change type, delete...)    #
+  // #############################################################
+  function GlassControl($elem) {
+    this.c = {'elem': $elem};
+
+    this.attachToModule = function($module) {
+      this.c.elem.fadeIn().insertBefore($module.element());
+    }
+
+    this.element = function() {
+      return this.c.elem;
+    };
+  }
 
   $(document).on('content-ready', function (e, element) {
     var $glass_editables = $(element).find('.glass-edit');
+
     var $form = null;
-
     $glass_editables.each(function () {
-      var options = get_glass_options($(this));
+      var $chunk  = $(this).glassChunk();
+      $chunk.makeEditable();
 
-      if (options['type'] == 'text') {
-        $(this).attr('contenteditable', true);
-      }
-      else if (options['type'] == 'html') {
-        // each child glass-module is editable
-      }
-
-      // Sync up all places where this attribute is used (if more than 1)
-      $(this).keyup(handle_change);
-
-      if (!$form && get_form_element($(this))) {
-        $form = get_form_element($(this)).parents('form');
+      if ($chunk.getForm()) {
+        $form = $chunk.getForm();
       }
     });
 
     if ($form) {
       $form.submit(function (e) {
         $('.glass-edit').each(function () {
-          var $form_elem = get_form_element($(this));
-          if ($form_elem) {
-            $form_elem.val(formatHtml($(this)));
-          }
+          $(this).glassChunk().setFormValFromHtml();
         });
       });
     }
   });
-
-  var handle_change = function(e) {
-    // Sync up all places where this attribute is used (if more than 1)
-  }
-
-  var get_form_element = function($element) {
-    var options = get_glass_options($element);
-    return ('form_id' in options) ? $('#' + options['form_id']) : null;
-  }
-
-  // Return the glass editing options (default is 'text', like a fancy <input type="text">)
-  var get_glass_options = function($element) {
-    var opt_str = $element.attr('data-glass-options');
-    return $.extend({'type': 'text'}, opt_str ? JSON.parse(opt_str) : {});
-  }
-
-
-  // #############################################################
-  // # Edit and Add HTML content                                 #
-  // #############################################################
-  $(document).on('content-ready', function (e, element) {
-    function insert_new_module_after($hook) {
-      var $new_module = $('#glass-parking .glass-module.blank').clone();
-      $new_module.removeClass('blank');
-      $new_module.prepend($('#glass-change-module').show());
-      $hook.after($new_module);
-      $hook.after("\n  ");
-      $(document).trigger('content-ready', $new_module[0]);
-      return $new_module;
-    }
-
-    var handle_keypress = function(e) {
-      // What non 'p' do we need to worry about?
-      // $(this).find('p').is(":focus") &&
-      if (e.which == 13) {
-        e.preventDefault();
-        var $new_module = insert_new_module_after($(this));
-        $new_module.find('p').first().focus();
-      }
-
-      // if ($(this).glass().isEmpty() && backspace) remove module
-    }
-
-    $(element).find('.glass-module').keypress(handle_keypress);
-    if ($(element).hasClass('glass-module')) {
-      $(element).keypress(handle_keypress);
-    }
-
-    $(element).find('#glass-blank-module-hook').each(function () {
-      var $new_module = insert_new_module_after($(this));
-      $(this).remove();
-    });
-
-
-    $(element).find('[contenteditable=true]').each(function () {
-      $(this).focusout(function () {
-        if (!$(this).text().trim()) {
-          $(this).html(''); // For FF, it puts a <br type='-moz'> in there
-        }
-      });
-    });
-    $(element).find('#glass-change-module').click(function (e) {
-      e.preventDefault();
-      replaceModuleContent($(this), '#glass-choose-module');
-    });
-    $(element).find('#glass-choose-module-vid').click(function (e) {
-      e.preventDefault();
-      replaceModuleContent($(this), '#glass-choose-vid');
-    });
-    $(element).find('.glass-module-close, #glass-choose-module-p').click(function (e) {
-      e.preventDefault();
-      var $module = $(this).parents('.glass-module');
-      $module.find('.glass-control').appendTo('#glass-parking');
-      $module.children().each(function () {
-        $(this).fadeIn();
-      });
-    });
-  });
-
-  function replaceModuleContent($me, selector) {
-    var $module = $me.parents('.glass-module');
-    var $replacement = $(selector);
-    $module.children().each(function () {
-      $(this).hide();
-    });
-    $module.append($replacement);
-    $replacement.fadeIn();
-  }
-
-  function formatHtml($elem) {
-    $elem.find('[contenteditable=true]').each(function () {
-      $(this).attr('contenteditable', false);
-    });
-    $elem.find('.glass-control').appendTo('#glass-parking');
-    return $elem.html().trim();
-  }
 
   // Return API for other modules
   return {};
