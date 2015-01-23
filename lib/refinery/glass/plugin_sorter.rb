22f258d2cbbae77d389d511b7a411a9ecfd461d7
Stefan <stefan@glasscanvascreative.com>
2015-01-23 11:49:36 -0800

    Refactor menu positions & put menu icons in

--- lib/refinery/glass/plugin_sorter.rb
+++ lib/refinery/glass/plugin_sorter.rb
@@ -1,37 +1,36 @@
 Refinery::Plugin.class_eval do
+  @@plugin_meta = {
+    "refinery_dashboard"    => {position: 5 , icon: 'gcicon gcicon-dashboard'},
+    "refinery_pages"        => {position: 10, icon: 'gcicon gcicon-pages'    },
+    "refinerycms_blog"      => {position: 15, icon: 'gcicon gcicon-feather'  },
+    "refinerycms_inquiries" => {position: 85, icon: 'gcicon gcicon-group'    },
+    "refinery_users"        => {position: 90, icon: 'gcicon gcicon-group'    },
+    "refinery_settings"     => {position: 0 , icon: 'gcicon gcicon-wrench'   }, #hide
+    "refinery_images"       => {position: 0 , icon: 'gcicon gcicon-wrench'   }, #hide
+    "refinery_files"        => {position: 0 , icon: 'gcicon gcicon-wrench'   }, #hide
+  }
+
+  @@plugin_meta.default     =  {position: 50, icon: 'gcicon gcicon-wrench'}
+
   def position
     positions_override = Refinery::Core.config.backend_menu_positions
-    if positions_override.present? && positions_override.has_key?(self.name)
-      return positions_override[self.name]
-    end
-
-    if @position
-      return @position
-    end
-
-    case self.name
-    when "refinery_dashboard"
-      return 5
-    when "refinery_pages"
-      return 10
-    when "refinerycms_blog"
-      return 15
-    when "refinerycms_inquiries"
-      return 85
-    when "refinery_users"
-      return 90
-    when "refinery_settings"
-      return 0 #hide
-    when "refinery_images"
-      return 0 #hide
-    when "refinery_files"
-      return 0 #hide
-    else
-      return 50
-    end
+    return positions_override[self.name] if (positions_override.present? && positions_override.has_key?(self.name))
+    return @position                     if @position
+    return @@plugin_meta[self.name][:position]
   end
 
   def position=(val)
     @position = val
+  end
+
+  def icon
+    icons_override = Refinery::Core.config.backend_menu_icons
+    return icons_override[self.name] if icons_override.present? && icons_override.has_key?(self.name)
+    return @icon_str                 if @icon_str
+    return @@plugin_meta[self.name][:icon]
+  end
+
+  def icon=(val)
+    @icon_str = val
   end
 end
