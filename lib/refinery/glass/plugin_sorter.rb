9ebc31b9cf1961fe9a54e94be1bf6463bab9c7db
Stefan <stefan@glasscanvascreative.com>
2015-01-26 10:24:17 -0800

    Fix warning in plugin sorter (menu order)
     - Use a constant instead of a class variable

--- lib/refinery/glass/plugin_sorter.rb
+++ lib/refinery/glass/plugin_sorter.rb
@@ -1,22 +1,22 @@
+Refinery::Plugin::META = {
+  "refinery_dashboard"    => {position: 5 , icon: 'gcicon gcicon-dashboard'},
+  "refinery_pages"        => {position: 10, icon: 'gcicon gcicon-pages'    },
+  "refinerycms_blog"      => {position: 15, icon: 'gcicon gcicon-feather'  },
+  "refinerycms_inquiries" => {position: 85, icon: 'gcicon gcicon-group'    },
+  "refinery_users"        => {position: 90, icon: 'gcicon gcicon-group'    },
+  "refinery_settings"     => {position: 0 , icon: 'gcicon gcicon-wrench'   }, #hide
+  "refinery_images"       => {position: 0 , icon: 'gcicon gcicon-wrench'   }, #hide
+  "refinery_files"        => {position: 0 , icon: 'gcicon gcicon-wrench'   }, #hide
+}
+
+Refinery::Plugin::META.default     =  {position: 50, icon: 'gcicon gcicon-wrench'}
+
 Refinery::Plugin.class_eval do
-  @@plugin_meta = {
-    "refinery_dashboard"    => {position: 5 , icon: 'gcicon gcicon-dashboard'},
-    "refinery_pages"        => {position: 10, icon: 'gcicon gcicon-pages'    },
-    "refinerycms_blog"      => {position: 15, icon: 'gcicon gcicon-feather'  },
-    "refinerycms_inquiries" => {position: 85, icon: 'gcicon gcicon-group'    },
-    "refinery_users"        => {position: 90, icon: 'gcicon gcicon-group'    },
-    "refinery_settings"     => {position: 0 , icon: 'gcicon gcicon-wrench'   }, #hide
-    "refinery_images"       => {position: 0 , icon: 'gcicon gcicon-wrench'   }, #hide
-    "refinery_files"        => {position: 0 , icon: 'gcicon gcicon-wrench'   }, #hide
-  }
-
-  @@plugin_meta.default     =  {position: 50, icon: 'gcicon gcicon-wrench'}
-
   def position
     positions_override = Refinery::Core.config.backend_menu_positions
     return positions_override[self.name] if (positions_override.present? && positions_override.has_key?(self.name))
     return @position                     if @position
-    return @@plugin_meta[self.name][:position]
+    return Refinery::Plugin::META[self.name][:position]
   end
 
   def position=(val)
@@ -27,7 +27,7 @@ Refinery::Plugin.class_eval do
     icons_override = Refinery::Core.config.backend_menu_icons
     return icons_override[self.name] if icons_override.present? && icons_override.has_key?(self.name)
     return @icon_str                 if @icon_str
-    return @@plugin_meta[self.name][:icon]
+    return Refinery::Plugin::META[self.name][:icon]
   end
 
   def icon=(val)
