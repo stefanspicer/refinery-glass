3a8e2e8f36a274c2e0c37b47a0be983a84bf1c7a
Joseph Krump <krump.joseph@gmail.com>
2015-01-30 13:09:16 -0800

    added migration for adding confirmable fields to users

---
+++ db/migrate/20150130204501_add_confirmable_to_devise_for_users.rb
@@ -0,0 +1,11 @@
+class AddConfirmableToDeviseForUsers < ActiveRecord::Migration
+  def change
+    change_table(:refinery_users) do |t|
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email
+    end
+    add_index  :refinery_users, :confirmation_token, :unique => true
+  end
+end
