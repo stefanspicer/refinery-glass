75986a315f55b2aa025d1aa2bee2f5571e40075c
Stefan <stefan@glasscanvascreative.com>
2015-02-09 09:44:29 -0800

    Inquiries decorator - only load if inquiries exist

--- app/decorators/controllers/refinery/inquiries/admin/inquiries_controller_decorator.rb
+++ app/decorators/controllers/refinery/inquiries/admin/inquiries_controller_decorator.rb
@@ -1,14 +1,16 @@
-Refinery::Inquiries::Admin::InquiriesController.class_eval do
+if defined?(Refinery::Inquiries::Admin::InquiriesController) == 'constant'
+  Refinery::Inquiries::Admin::InquiriesController.class_eval do
 
-	def index
-		if params[:search]
-			@inquiries = Refinery::Inquiries::Inquiry.where("name LIKE '%#{params[:search]}%' OR message LIKE '%#{params[:search]}%'")
-		else
-			find_all_inquiries
-		end
+    def index
+      if params[:search]
+        @inquiries = Refinery::Inquiries::Inquiry.where("name LIKE '%#{params[:search]}%' OR message LIKE '%#{params[:search]}%'")
+      else
+        find_all_inquiries
+      end
 
-		paginate_all_inquiries
+      paginate_all_inquiries
 
-	end
+    end
 
+  end
 end
