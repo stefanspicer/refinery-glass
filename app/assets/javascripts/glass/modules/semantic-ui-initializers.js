01e42b197b783746deb91772968b617344023013
Joseph Krump <krump.joseph@gmail.com>
2015-03-17 17:07:02 -0700

    removed attempt to initialize semantic-ui popup

--- app/assets/javascripts/glass/modules/semantic-ui-initializers.js
+++ app/assets/javascripts/glass/modules/semantic-ui-initializers.js
@@ -1,15 +1,7 @@
 var GlassSemanticUIinitializers = (function ($){
 
   $(document).on('content-ready', function (e, element) {
-    $('.default-popup').popup({
-      inline: true,
-      hoverable: true
-    });
-    $('input.popup').popup({
-      on: 'focus',
-      preserve: true,
-      hoverable: true
-    });
+
   });
 
   // Return API for other modules
