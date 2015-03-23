387371747750227d03675a4d6ccf9c1b504cf287
Joseph Krump <krump.joseph@gmail.com>
2015-03-23 16:45:24 -0700

    updated pagination to fade in and out the loading spinner div

--- app/assets/javascripts/glass/modules/pagination.js
+++ app/assets/javascripts/glass/modules/pagination.js
@@ -8,38 +8,56 @@ var GlassPagination = (function ($) {
   $(document).on('content-ready', function (e, element) {
     var $showMoreButton = $('.btn-paginate-show-more');
     var $infiniteScrollContainer = $('.infinite-scrolling-container');
+    var $paginationSpinner = $('.pagination-spinner');
 
     if($showMoreButton.length > 0){
       $showMoreButton.click(function(e){
         e.preventDefault();
-        var moreUrl = $(this).attr('data-url');
-        $.getScript(moreUrl);
+        if(!FETCHING){
+          var moreUrl = $(this).attr('data-url');
+          displaySpinner($paginationSpinner);
+          FETCHING = true;
+          $.getScript(moreUrl);
+        }
       });
     }
 
-    if ($infiniteScrollContainer.length > 0){
-      $(document).on('pagination-content-loaded', function(e, element) {
-        FETCHING = false;
-      });
+    {
 
+    if ($infiniteScrollContainer.length > 0)
       $(window).on('scroll', scrollHandler);
-
     }
+
+    $(document).on('pagination-content-loaded', function(e, element) {
+      FETCHING = false;
+      $paginationSpinner.fadeOut(200);
+    });
+
   });
 
   function scrollHandler(){
     var moreUrl = $('.infinite-scrolling-container').attr('data-url');
     var pixelOffsetFromBottom = 1200;
+    var $paginationSpinner = $('.pagination-spinner');
 
     if(moreUrl && ($(window).scrollTop() > ($(document).height() - $(window).height() - pixelOffsetFromBottom))){
       if(!FETCHING){
         FETCHING = true;
+        displaySpinner($paginationSpinner);
         $.getScript(moreUrl);
       }
     }
   }
 
-
+  /**
+   *
+   * @param $paginationSpinner
+   */
+  function displaySpinner($paginationSpinner){
+    if($paginationSpinner.length > 0){
+      $paginationSpinner.fadeIn(200);
+    }
+  }
 
   return {
     scrollHandler: scrollHandler
