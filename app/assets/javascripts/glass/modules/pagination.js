08c1d6770172cb569e7ed898fcc608f3f425b21c
Joseph Krump <krump.joseph@gmail.com>
2015-03-23 16:02:06 -0700

    pagination added

--- app/assets/javascripts/glass/modules/pagination.js
+++ app/assets/javascripts/glass/modules/pagination.js
@@ -3,23 +3,51 @@
  */
 
 var GlassPagination = (function ($) {
-  $(document).on('content-ready', function (e, element) {
-    $('.btn-paginate-show-more').click(function(e){
-      e.preventDefault();
-      var moreUrl = $(this).data('url');
-      $.getScript(moreUrl);
-    });
+  var FETCHING = false;
 
-    if ($('.infinite-scrolling-container').length > 0){
-      $(window).on('scroll', function(e){
-        var moreUrl = $(this).data('url');
-        if(moreUrl && ($(window).scrollTop() > ($(document).height() - $(window).height() - 60))){
-          $.getScript(moreUrl);
-        }
+  $(document).on('content-ready', function (e, element) {
+    var $showMoreButton = $('.btn-paginate-show-more');
+    var $infiniteScrollContainer = $('.infinite-scrolling-container');
+
+    if($showMoreButton.length > 0){
+      $showMoreButton.click(function(e){
+        e.preventDefault();
+        var moreUrl = $(this).attr('data-url');
+        $.getScript(moreUrl);
       });
+    }
+
+
+    if ($infiniteScrollContainer.length > 0){
+      $(document).on('pagination-content-loaded', function(e, element) {
+        FETCHING = false;
+        console.log('reset fetching');
+      });
+
+      $(window).on('scroll', scrollHandler);
+
     }
   });
 
-  return {}
+  function scrollHandler(){
+    var moreUrl = $('.infinite-scrolling-container').attr('data-url');
+    var pixelOffsetFromBottom = 400;
+
+    if(moreUrl && ($(window).scrollTop() > ($(document).height() - $(window).height() - pixelOffsetFromBottom))){
+      if(!FETCHING){
+        console.log('not fetching');
+        FETCHING = true;
+        $.getScript(moreUrl);
+      } else {
+        console.log('fetching');
+      }
+    }
+  }
+
+
+
+  return {
+    scrollHandler: scrollHandler
+  }
 
 })(jQuery);
