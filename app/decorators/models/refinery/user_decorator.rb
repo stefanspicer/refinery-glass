df498ba739e9f2d8a8231833e0885240338edd05
Joseph Krump <krump.joseph@gmail.com>
2015-03-26 11:55:20 -0700

    reset password form override added

--- app/decorators/models/refinery/user_decorator.rb
+++ app/decorators/models/refinery/user_decorator.rb
@@ -1,6 +1,6 @@
 Refinery::User.class_eval do
 
-  devise :database_authenticatable, :confirmable
+  devise :database_authenticatable, :confirmable, :recoverable
 
   attr_accessible :confirmed_at, :confirmation_token, :inviting_user
   attr_accessor   :inviting_user
