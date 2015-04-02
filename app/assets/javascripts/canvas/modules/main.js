641ed216e3016dbb439ead7b34c684349772b47e
Joseph Krump <krump.joseph@gmail.com>
2015-04-02 09:12:24 -0700

    removed unique selector method in main and removed console.logs

--- app/assets/javascripts/canvas/modules/main.js
+++ app/assets/javascripts/canvas/modules/main.js
@@ -5,30 +5,8 @@ var Main = (function($){
     });
   });
 
-  var getUniqueSelector = function (node) {
-    if (node.length !== 1) throw 'Requires one element.';
-
-    var path;
-    while (node.length) {
-      var realNode = node[0], name = realNode.localName;
-      if (!name) break;
-      name = name.toLowerCase();
-
-      var parent = node.parent();
-
-      var siblings = parent.children(name);
-      if (siblings.length > 1) {
-        name += ':eq(' + siblings.index(realNode) + ')';
-      }
-
-      path = name + (path ? '>' + path : '');
-      node = parent;
-    }
-
-    return path;
-  };
-
   function contentReady(element) {
+
     $(document).trigger('content-ready', element);
     $('.btn-anchor').removeAttr('disabled');
     btnAnchorInitialization(element);
@@ -66,7 +44,6 @@ var Main = (function($){
       } else {
         window.location.href = $btn.attr('data-url');
       }
-
     });
 
     var $selectInput = $(element).find('.select-anchors');
